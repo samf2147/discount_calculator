aa01795bb8ea699dde2f9f986f2727d97e1c4f44
Sam Frank <sfrank2147@gmail.com>
2014-01-01 13:47:15 -0600

    added tests for invalid discounts

--- discount_calculator.py
+++ discount_calculator.py
@@ -3,10 +3,41 @@ class DiscountCalculator():
     def __init__(self):
         pass
     
-    def calculate_discount(self, original, discount, type):
-        if type == 'percent':
-            return 0.01 * original * discount
-        elif type == 'absolute':
-            return discount
+    def calculate_discount(self, original, discount, discount_type):
+        discount_value = None
+        
+        #handle percent case
+        if discount_type == 'percent':
+            if not self._valid_percent(discount):
+                raise ValueError
+            else:
+                discount_value = 0.01 * original * discount
+                
+        #handle absolute case
+        elif discount_type == 'absolute':
+            if not self._valid_absolute(original, discount):
+                raise ValueError
+            else:
+                discount_value = discount
+            
+        #handle invalid discount type
         else:
-            return None
+            raise ValueError
+        return discount_value
+    
+    def _valid_percent(self, percent):
+        '''
+        Returns True iff percent is between 0 and 100
+        Don't allow 0% discounts or 100% discounts
+        '''
+        if percent > 0 and percent <= 100:
+            valid = True
+        else:
+            valid = False
+        return valid
+
+    def _valid_absolute(self, original, discount):
+        if discount > 0 and discount <= original:
+            return True
+        else:
+            return False
--- discount_tests.py
+++ discount_tests.py
@@ -14,3 +14,27 @@ class DiscountTests(unittest.TestCase):
     
     def test_ten_dollar_discount(self):
         self.assertEqual(self.discount.calculate_discount(100,10.0,'absolute'),10)
+        
+    def test_invalid_discount_type_error(self):
+        self.assertRaises(ValueError, self.discount.calculate_discount, 60, 20, 'random string')
+    
+    def test_invalid_percents(self):
+        #over 100% or less than 0%
+        self.assertRaises(ValueError, self.discount.calculate_discount, 100, 120, 'percent')
+        self.assertRaises(ValueError, self.discount.calculate_discount,100,-20,'percent')
+        
+        #0% is not ok, 100% is
+        self.assertRaises(ValueError,self.discount.calculate_discount,30,0,'percent')
+        self.assertEqual(self.discount.calculate_discount(50, 100, 'percent'), 50)
+    
+    def test_invalid_absolutes(self):
+        #absolute discount can't be less than 0 or more than the cost of the item
+        #discount of 0 is not ok, full discount is ok
+        #over 100% or less than 0%
+        self.assertRaises(ValueError, self.discount.calculate_discount, 70, 80, 'absolute')
+        self.assertRaises(ValueError, self.discount.calculate_discount,70,-10,'absolute')
+        
+        #0% is not ok, 100% is
+        self.assertRaises(ValueError,self.discount.calculate_discount,40,0.0,'absolute')
+        self.assertEqual(self.discount.calculate_discount(60, 60, 'absolute'), 60)
+        
